44d92d1a6094ff93b5923fa5079835cf1d8fa00e
Anthony Pinter <anthony.pinter@colorado.edu>
2023-08-01 22:20:04 -0600

    functioning script (<100 songs/playlist)

--- search.py
+++ search.py
@@ -26,7 +26,7 @@ limit_count = 0
 
 ###################
 # IMPORTANT VARIABLES 
-limit = 50 # this controls how many results you get; min is 1, max is 50, default is 10
+limit = 2 # this controls how many results you get; min is 1, max is 50, default is 10
 search_term = 'funeral'
 ###################
 
@@ -38,7 +38,7 @@ playlist = sp.search(search_term, limit=limit, offset=0, type='playlist', market
 ## type is the type of thing being search for
 ## market is used if you want to constrain to a particular countryt
 
-out_columns = ['PlaylistName', 'PlaylistURL', 'PlaylistID', 'Owner', 'OwnerURL', 'Number of Tracks', 'Description', 'Collaborative', 'ImageURL']
+out_columns = ['PlaylistName', 'PlaylistURL', 'PlaylistID', 'Number','Owner', 'OwnerURL', 'Number of Tracks', 'Description', 'Collaborative', 'ImageURL']
 out = pd.DataFrame(columns=out_columns)
 
 while limit_count < limit:
@@ -55,17 +55,20 @@ while limit_count < limit:
   playlist_url = playlist['playlists']['items'][limit_count]['external_urls']['spotify']
   image_url = playlist['playlists']['items'][limit_count]['images'][0]['url']
   track_url = playlist['playlists']['items'][limit_count]['tracks']['href']
-  print('*****************')
+  #print('*****************')
 
   playlist_id = playlist['playlists']['items'][limit_count]['id']
 
-  playlist_info = [playlist_name, playlist_url, playlist_id, owner, owner_url, num_tracks, description, collaborative, image_url]
+  number = playlist['playlists']['items'][limit_count]['tracks']['total']
+
+  playlist_info = [playlist_name, playlist_url, playlist_id, number, owner, owner_url, num_tracks, description, collaborative, image_url]
 
   out.loc[len(out.index)] = playlist_info
-  print(out)
+  #print(out)
   out.to_csv('out.csv')
   out.to_json('out.json')
   #print(sp.playlist_items(playlist_id, limit=1))
+
   
   #print(playlist['playlists']['items'][limit_count]['name'])
   #print(playlist['playlists']['items'][limit_count]['description'])
@@ -74,3 +77,77 @@ while limit_count < limit:
 
 
 #then, we'll cycle through the out and use the playlist ID value to get the tracks from each playlist.
+
+song_columns = ['PlaylistID', 'Song', 'SongURI', 'PreviewLink', 'Album', 'Artist', 'BigAlbumArt', 'SmallAlbumArt', 'Danceability', 'Energy', 'Key', 'Loudness', 'Mode', 'Speechiness', 'Acousticness', 'Instrumentalness', 'Liveness', 'Valence', 'Tempo', 'Duration', 'TimeSignature']
+song_df = pd.DataFrame(columns=song_columns)
+
+for index, row in out.iterrows():
+  pid = out['PlaylistID'].loc[out.index[index]]
+  size = out['Number'].loc[out.index[index]] ## this will be a problem if the number is greater than 100
+  
+  y = 0
+
+  while y < size:
+    x = (sp.playlist_items(pid, limit=100))
+    song_id = x['items'][y]['track']['id']
+
+    y +=1
+
+    results = sp.track(song_id)
+        
+    artist_name = results['album']['artists'][0]['name']
+    album_name = results['album']['name']
+    big_album_art = results['album']['images'][1]['url']
+    small_album_art = results['album']['images'][2]['url']
+    song_name = results['name']
+    song_uri = results['uri']
+    preview_link = results['preview_url']
+
+    #audio_analysis = sp.audio_analysis(song_uri)
+    # probably don't need this ^
+
+    audio_features = sp.audio_features(song_uri)
+
+    danceability = audio_features[0]['danceability']
+    energy = audio_features[0]['energy']
+    key = audio_features[0]['key']
+    loudness = audio_features[0]['loudness']
+    mode = audio_features[0]['mode']
+    speechiness = audio_features[0]['speechiness']
+    acousticness = audio_features[0]['acousticness']
+    instrumentalness = audio_features[0]['instrumentalness']
+    liveness = audio_features[0]['liveness']
+    valence = audio_features[0]['valence']
+    tempo = audio_features[0]['tempo']
+    duration = audio_features[0]['duration_ms']
+    time_signature = audio_features[0]['time_signature']
+
+
+    ## Leaving this here for now... just in case.
+    # ------------------------- GENIUS LYRIC PULL
+
+    #artist = genius.search_artist("Say Anything", max_songs=5)
+    #song = artist.song("Alive With the Glory of Love")
+    #artist = genius.search_artist("Say Anything", max_songs=5)
+    #try:
+            #song = genius.search_song(song_name, artist_name)
+            #lyrics_new_line_breaks = song.lyrics
+            #lyrics = lyrics_new_line_breaks.replace("\n", " ")
+    #except:
+            #lyrics = "NO LYRICS"
+    #print(lyrics_new_line_breaks)
+
+    
+    #print(lyrics)
+
+    #song.save_lyrics()  
+
+    # ------------------------- OUT DF COMPOSITION
+
+    song_array = [pid, song_name, song_uri, preview_link, album_name, artist_name, big_album_art, small_album_art, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo, duration, time_signature]
+
+    song_df.loc[len(song_df.index)] = song_array
+
+song_df.to_csv('songdf.csv')
+song_df.to_json('songdf.json')
+    
