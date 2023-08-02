1dbeac6fcb2c05c9aa18ec7db6afd36da741a0c3
Anthony Pinter <anthony.pinter@colorado.edu>
2023-08-01 18:18:26 -0600

    playlist search working

--- search.py
+++ search.py
@@ -21,17 +21,22 @@ sp = spotipy.Spotify(
           redirect_uri='http://localhost/',    
           scope=scope, open_browser=False))
 
-limit = 1
+limit_count = 0
 
-playlist = sp.search("funeral", limit=limit, offset=0, type='playlist', market=None)
+
+###################
+# IMPORTANT VARIABLES 
+limit = 50 # this controls how many results you get; min is 1, max is 50, default is 10
+search_term = 'funeral'
+###################
+
+playlist = sp.search(search_term, limit=limit, offset=0, type='playlist', market=None)
 # search(q, limit, offset, type, market)
 ## q is the search term or terms
 ## limit is the number of results requested
 ## offset moves the index of the first result returned
 ## type is the type of thing being search for
 ## market is used if you want to constrain to a particular countryt
-
-limit_count = 0
 
 out_columns = ['PlaylistName', 'PlaylistURL', 'PlaylistID', 'Owner', 'OwnerURL', 'Number of Tracks', 'Description', 'Collaborative', 'ImageURL']
 out = pd.DataFrame(columns=out_columns)
@@ -41,27 +46,31 @@ while limit_count < limit:
 
   #[{'collaborative': False, 'description': 'Ryuichi’s Last Playlist. We would like to share the playlist that Ryuichi had been privately compiling to be played at his own funeral to accompany his own passing. He truly was with music until the very end.', 'external_urls': {'spotify': 'https://open.spotify.com/playlist/31OIme0YdF4ORWvEdTyE6V'}, 'href': 'https://api.spotify.com/v1/playlists/31OIme0YdF4ORWvEdTyE6V', 'id': '31OIme0YdF4ORWvEdTyE6V', 'images': [{'height': None, 'url': 'https://images-ak.spotifycdn.com/image/ab67706c0000bebbe0a0dbec04a4ccd67b95e647', 'width': None}], 'name': 'funeral', 'owner': {'display_name': 'Ryuichi Sakamoto', 'external_urls': {'spotify': 'https://open.spotify.com/user/ryuichisakamoto'}, 'href': 'https://api.spotify.com/v1/users/ryuichisakamoto', 'id': 'ryuichisakamoto', 'type': 'user', 'uri': 'spotify:user:ryuichisakamoto'}, 'primary_color': None, 'public': None, 'snapshot_id': 'OTcsNTY3YWY4NDgzODdkYTJiOTAzYzJjMjQ2NTY3ZGQyN2FkOWM3MGE0OA==', 'tracks': {'href': 'https://api.spotify.com/v1/playlists/31OIme0YdF4ORWvEdTyE6V/tracks', 'total': 33}, 'type': 'playlist', 'uri': 'spotify:playlist:31OIme0YdF4ORWvEdTyE6V'}]
 
-  playlist = playlist['playlists']['items'][limit_count]['name']
+  playlist_name = playlist['playlists']['items'][limit_count]['name']
   owner = playlist['playlists']['items'][limit_count]['owner']['display_name']
   owner_url = playlist['playlists']['items'][limit_count]['owner']['external_urls']['spotify']
   num_tracks = playlist['playlists']['items'][limit_count]['tracks']['total']
   description = playlist['playlists']['items'][limit_count]['description']
   collaborative = playlist['playlists']['items'][limit_count]['collaborative']
-  playlist_url = ['playlists']['items'][limit_count]['external_urls']['spotify']
+  playlist_url = playlist['playlists']['items'][limit_count]['external_urls']['spotify']
   image_url = playlist['playlists']['items'][limit_count]['images'][0]['url']
   track_url = playlist['playlists']['items'][limit_count]['tracks']['href']
   print('*****************')
 
   playlist_id = playlist['playlists']['items'][limit_count]['id']
 
-  playlist_info = [limit_count, playlist, playlist_url, playlist_id, owner, owner_url, num_tracks, description, collaborative, image_url]
+  playlist_info = [playlist_name, playlist_url, playlist_id, owner, owner_url, num_tracks, description, collaborative, image_url]
 
-  
-
-  print(sp.playlist_items(playlist_id, limit=1))
+  out.loc[len(out.index)] = playlist_info
+  print(out)
+  out.to_csv('out.csv')
+  out.to_json('out.json')
+  #print(sp.playlist_items(playlist_id, limit=1))
   
   #print(playlist['playlists']['items'][limit_count]['name'])
   #print(playlist['playlists']['items'][limit_count]['description'])
   #print(playlist['playlists']['items'][limit_count]['external_urls']['spotify'])
   limit_count += 1
-#out.to_csv('playlist-out1.csv')
+
+
+#then, we'll cycle through the out and use the playlist ID value to get the tracks from each playlist.
